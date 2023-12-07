cb89b3ed33593695d591455cc2c361d770dd6eeb
DobloS <s.maks0208@mail.ru>
2023-12-07 20:00:44 +0500

    Решение Task2

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Practice
@@ -12,6 +15,17 @@ namespace Practice
         static void Main(string[] args)
         {
             string str = Convert.ToString(Console.ReadLine());
+
+            if (!CheckAlphabet(str))
+            {
+                string a = "";
+                foreach (var item in str)
+                {
+                    a += CheckAlphabet(item);
+                }
+                throw new AlphabetException("Неподходящие символы " + a);
+            }
+           
             if (str.Length % 2 == 0)
             {
                 string firstHalf = str.Substring(0, str.Length / 2);
@@ -23,6 +37,7 @@ namespace Practice
                 string reversedString = new string(firstHalfArray) + new string(secondHalfArray);
                 Console.WriteLine(reversedString);
             }
+
             else
             {
                 char[] strArray = str.ToCharArray();
@@ -30,7 +45,31 @@ namespace Practice
                 string resultString = new string(strArray) + str;
                 Console.WriteLine(resultString);
             }
-
         }
+
+        public static bool CheckAlphabet(string str)
+        {
+            Regex regex = new Regex("^[a-z]*$");
+            bool fls = regex.IsMatch(str);
+            return fls;
+        }
+        public static char CheckAlphabet(char c)
+        {
+            Regex regex = new Regex("^[a-z]*$");
+            if (!regex.IsMatch(c.ToString()))
+            {
+                return c;
+            }
+            else
+            {
+                return ' ';
+            }
+        }
+
+    }
+
+    class AlphabetException : Exception
+    {
+        public AlphabetException(string message): base(message) { }
     }
 }
