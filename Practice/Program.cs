8615a66744fede47946899a3e1a6c496e84e933f
DobloS <s.maks0208@mail.ru>
2023-12-07 17:44:11 +0500

    Добавьте файлы проекта.

---
+++ Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Convert.ToString(Console.ReadLine());
+            string[] words = str.Split(new char[] { ' ' });
+            int a = str.Length;
+            if (a / 2 == 0)
+            {
+                str = str.Substring(a/2);
+                
+
+            }
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine(words);
+
+
+        }
+    }
+}
