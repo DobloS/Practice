aa64df67ff835c6dd21232d50a1643bfc724d002
DobloS <s.maks0208@mail.ru>
2023-12-08 18:56:30 +0500

    Решение

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -19,37 +19,70 @@ namespace Practice
 
             if (!CheckAlphabet(str))
             {
-                string a = "";
+                string temp = "";
                 foreach (var item in str)
                 {
-                    a += CheckAlphabet(item);
+                    temp += CheckAlphabet(item);
                 }
-                throw new AlphabetException("Неподходящие символы " + a);
+                throw new AlphabetException("Неподходящие символы: " + temp);
             }
            
             if (str.Length % 2 == 0)
             {
                 string firstHalf = str.Substring(0, str.Length / 2);
                 string secondHalf = str.Substring(str.Length / 2);
+
                 char[] firstHalfArray = firstHalf.ToCharArray();
                 Array.Reverse(firstHalfArray);
                 char[] secondHalfArray = secondHalf.ToCharArray();
                 Array.Reverse(secondHalfArray);
+
                 string reversedString = new string(firstHalfArray) + new string(secondHalfArray);
                 Console.WriteLine(reversedString);
-                a(reversedString);
+
+                GetCountChar(reversedString);
+
+                string temp = FindLargestSubstring(reversedString);
+                Console.WriteLine("Наибольшая подстрока начинающаяся и оканчивающаяся на гласную: " + temp);
             }
 
             else
             {
                 char[] strArray = str.ToCharArray();
                 Array.Reverse(strArray);
+
                 string resultString = new string(strArray) + str;
                 Console.WriteLine(resultString);
-                a(resultString);
+
+                GetCountChar(resultString);
+
+                string temp = FindLargestSubstring(resultString);
+                Console.WriteLine("Наибольшая подстрока начинающаяся и оканчивающаяся на гласную: " + temp);
             }
         }
-        public static void a(string str)
+
+        static string FindLargestSubstring(string s)
+        {
+            string vowels = "aeiouy";
+            string maxSubstring = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (vowels.Contains(s[i]) && vowels.Contains(s[j]))
+                    {
+                        string substring = s.Substring(i, j - i + 1);
+                        if (substring.Length > maxSubstring.Length)
+                        {
+                            maxSubstring = substring;
+                        }
+                    }
+                }
+            }
+            return maxSubstring;
+        }
+    
+        public static void GetCountChar(string str)
         {
             foreach (var item in str.Distinct().ToArray())
             {
@@ -64,12 +97,13 @@ namespace Practice
             bool fls = regex.IsMatch(str);
             return fls;
         }
-        public static char CheckAlphabet(char c)
+
+        public static char CheckAlphabet(char chr)
         {
             Regex regex = new Regex("^[a-z]*$");
-            if (!regex.IsMatch(c.ToString()))
+            if (!regex.IsMatch(chr.ToString()))
             {
-                return c;
+                return chr;
             }
             else
             {
